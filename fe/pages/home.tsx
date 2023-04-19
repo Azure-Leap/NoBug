f9af163120d05bb581bc06825c6ace7e7f0c93bf
Gankhulug <huluguu0202@gmail.com>
2023-04-19 17:06:09 +0800

    bug

--- fe/pages/home.tsx
+++ fe/pages/home.tsx
@@ -1,14 +1,8 @@
-import Navbar from "@/components/navbar";
-import PopularCat from "@/components/popularCat";
 import { Box } from "@mui/material";
 import React from "react";
 
 const Home = () => {
-  return (
-    <Box sx={{ width: { xs: "95%", xl: "75%" }, margin: "auto" }}>
-      <PopularCat />
-    </Box>
-  );
+  return <Box sx={{ width: { xs: "95%", xl: "75%" }, margin: "auto" }}></Box>;
 };
 
 export default Home;
