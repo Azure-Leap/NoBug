875df037bee523065b803f3ccc4204b36ef297f4
Jambalsuren0012 <naruto_0212@yahoo.com>
2023-04-25 10:57:05 +0800

    navbar bar zasav

--- fe/pages/index.tsx
+++ fe/pages/index.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/navbar";
+import NavBar from "@/components/navbar/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <div>Hello from the other siiiiiide</div>;
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
 }
