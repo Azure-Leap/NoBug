0f7ff04cd94a38735f29a77f76ed8db6f6b58df6
APPLE <apple@APPLEui-MacBookPro.local>
2023-04-25 15:13:06 +0800

    commendPart -iig hiilee.

--- fe/pages/home.tsx
+++ fe/pages/home.tsx
@@ -1,9 +1,14 @@
+import CommendPart from "@/components/commendPart";
 import ExpertFreelancer from "@/components/expertFreelander";
 import { Box } from "@mui/material";
 import React from "react";
 
 const Home = () => {
-  return <Box sx={{ width: { xs: "95%", xl: "75%" }, margin: "auto" }}></Box>;
+  return (
+    <Box sx={{ width: { xs: "95%", xl: "75%" }, margin: "auto" }}>
+      <CommendPart />
+    </Box>
+  );
 };
 
 export default Home;
