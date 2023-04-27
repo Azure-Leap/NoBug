8b038fa8b41fb1d784e8250aa4cfd11b65f6e1b7
APPLE <apple@APPLEui-MacBookPro.local>
2023-04-27 20:37:41 +0800

    logIn form hiij duusgalaa.

---
+++ fe/components/auth/signIn.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { useRouter } from "next/router";
+import styles from "./styles.module.css";
+import { FcGoogle } from "react-icons/fc";
+import { BsFacebook } from "react-icons/bs";
+
+function SignInForm() {
+  const router = useRouter();
+
+  const routeTo = (url: string) => {
+    router.replace(url);
+  };
+  const [state, setState] = React.useState({
+    email: "",
+    password: "",
+  });
+  const handleChange = (evt: any) => {
+    const value = evt.target.value;
+    setState({
+      ...state,
+      [evt.target.name]: value,
+    });
+  };
+
+  console.log("state", state);
+  const handleOnSubmit = (evt: any) => {
+    evt.preventDefault();
+
+    const { email, password } = state;
+    alert(`You are login with email: ${email} and password: ${password}`);
+
+    for (const key in state) {
+      setState({
+        ...state,
+        [key]: "",
+      });
+    }
+  };
+
+  return (
+    <div className={`${styles.formContainer} ${styles.signInContainer}`}>
+      <form onSubmit={handleOnSubmit} className={styles.form}>
+        <h1 className={styles.head1}>Sign in</h1>
+        <div className={styles.socialContainer}>
+          <a>
+            <FcGoogle
+              onClick={() => {
+                routeTo("https://mail.google.com/mail/u/0/#inbox");
+              }}
+              size={40}
+            />
+          </a>
+          <a>
+            <BsFacebook
+              onClick={() => {
+                routeTo("https://www.facebook.com/login/");
+              }}
+              size={40}
+              color={"#3b5998"}
+            />
+          </a>
+        </div>
+        <span className={styles.span1}>or use your account</span>
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          value={state.email}
+          onChange={handleChange}
+          className={styles.input}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={state.password}
+          onChange={handleChange}
+          className={styles.input}
+        />
+        <a href="#" className={styles.aLink}>
+          Forgot your password?
+        </a>
+        <button className={styles.btn}>Sign In</button>
+      </form>
+    </div>
+  );
+}
+
+export default SignInForm;
--- fe/pages/home.tsx
+++ fe/pages/home.tsx
@@ -1,24 +1,26 @@
+import Auth from "@/components/auth/logIn";
+import LogIn from "@/components/auth/logIn";
 import ExpertFreelancer from "@/components/expertFreelander";
 import { Box } from "@mui/material";
 import React from "react";
 
 const Home = ({ data }: any) => {
-  console.log(data.freelancer);
+  // console.log(data.freelancer);
   return (
-    // <Box sx={{ width: { xs: "95%", xl: "75%" }, margin: "auto" }}>
-    <ExpertFreelancer data={data.freelancer} />
-    // </Box>
+    <>
+      <Auth />
+    </>
   );
 };
 
-export async function getServerSideProps() {
-  const res = await fetch("http://localhost:8000/freelancer");
-  const data = await res.json();
-  return {
-    props: {
-      data,
-    },
-  };
-}
+// export async function getServerSideProps() {
+//   const res = await fetch("http://localhost:8000/freelancer");
+//   const data = await res.json();
+//   return {
+//     props: {
+//       data,
+//     },
+//   };
+// }
 
 export default Home;
