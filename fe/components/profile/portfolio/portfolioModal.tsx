31fabcc24c14691ffb7c55e597fa38674247465d
Gankhulug <huluguu0202@gmail.com>
2023-04-30 13:27:10 +0800

    profile page iig responsive bolgoloo

--- fe/components/profile/portfolio/portfolioModal.tsx
+++ fe/components/profile/portfolio/portfolioModal.tsx
@@ -68,114 +68,112 @@ export default function PortfolioModal({
   }, [active]);
 
   return (
-    <div>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Box
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <Box
+          sx={{
+            width: "100%",
+            height: "100%",
+            position: "relative",
+            p: { xs: 2, md: 6 },
+            pt: 8,
+          }}
+        >
+          <CloseIcon
+            onClick={handleClose}
             sx={{
-              width: "100%",
-              height: "100%",
-              position: "relative",
-              p: { xs: 2, md: 6 },
-              pt: 8,
+              position: "absolute",
+              top: "20px",
+              right: "40px",
+              color: "black",
+              ":hover": {
+                cursor: "pointer",
+              },
             }}
-          >
-            <CloseIcon
-              onClick={handleClose}
+          />
+          <ArrowForwardIosIcon
+            onClick={next}
+            sx={{
+              position: "absolute",
+              top: "40%",
+              right: "50px",
+              color: "black",
+              ":hover": {
+                cursor: "pointer",
+              },
+            }}
+          />
+          <ArrowBackIosIcon
+            onClick={previous}
+            sx={{
+              position: "absolute",
+              top: "40%",
+              left: "50px",
+              color: "black",
+              ":hover": {
+                cursor: "pointer",
+              },
+            }}
+          />
+          <Box sx={{ width: "100%", height: "100%" }}>
+            <Box sx={{ width: "100%", height: { xs: "70%", md: "80%" } }}>
+              <Image
+                src={data[active]}
+                alt={data[1]}
+                width={2000}
+                height={1000}
+                className="object-contain w-full h-full"
+              />
+            </Box>
+            <Box
               sx={{
-                position: "absolute",
-                top: "20px",
-                right: "40px",
-                color: "black",
-                ":hover": {
-                  cursor: "pointer",
-                },
+                display: "flex",
+                gap: { xs: "5px", md: "20px" },
+                width: "100%",
+                height: { xs: "30%", md: "20%" },
+                overflowX: "scroll",
+                py: 2,
               }}
-            />
-            <ArrowForwardIosIcon
-              onClick={next}
-              sx={{
-                position: "absolute",
-                top: "40%",
-                right: "50px",
-                color: "black",
-                ":hover": {
-                  cursor: "pointer",
-                },
-              }}
-            />
-            <ArrowBackIosIcon
-              onClick={previous}
-              sx={{
-                position: "absolute",
-                top: "40%",
-                left: "50px",
-                color: "black",
-                ":hover": {
-                  cursor: "pointer",
-                },
-              }}
-            />
-            <Box sx={{ width: "100%", height: "100%" }}>
-              <Box sx={{ width: "100%", height: { xs: "70%", md: "80%" } }}>
-                <Image
-                  src={data[active]}
-                  alt={data[1]}
-                  width={2000}
-                  height={1000}
-                  className="object-contain w-full h-full"
-                />
-              </Box>
-              <Box
-                sx={{
-                  display: "flex",
-                  gap: { xs: "5px", md: "20px" },
-                  width: "100%",
-                  height: { xs: "30%", md: "20%" },
-                  overflowX: "scroll",
-                  py: 2,
-                }}
-              >
-                {data.map((e: any, index: any) => (
-                  <>
-                    <Box
-                      onClick={() => {
-                        setActive(index);
+            >
+              {data.map((e: any, index: any) => (
+                <>
+                  <Box
+                    onClick={() => {
+                      setActive(index);
+                    }}
+                    sx={{
+                      width: { xs: "40%", md: "20%" },
+                      flex: "none",
+                    }}
+                  >
+                    <Image
+                      key={index}
+                      src={e}
+                      alt={e}
+                      width={200}
+                      height={200}
+                      style={{
+                        height: "100%",
+                        width: "100%",
+                        objectFit: "cover",
+                        filter:
+                          index === active
+                            ? "brightness(100%)"
+                            : "brightness(50%)",
                       }}
-                      sx={{
-                        width: { xs: "40%", md: "20%" },
-                        flex: "none",
-                      }}
-                    >
-                      <Image
-                        key={index}
-                        src={e}
-                        alt={e}
-                        width={200}
-                        height={200}
-                        style={{
-                          height: "100%",
-                          width: "100%",
-                          objectFit: "cover",
-                          filter:
-                            index === active
-                              ? "brightness(100%)"
-                              : "brightness(50%)",
-                        }}
-                      />
-                    </Box>
-                  </>
-                ))}
-              </Box>
+                    />
+                  </Box>
+                </>
+              ))}
             </Box>
           </Box>
         </Box>
-      </Modal>
-    </div>
+      </Box>
+    </Modal>
   );
 }
