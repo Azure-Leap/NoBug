b94492a7f71c3c8754e6209049f15b8dd7987b0a
Jambalsuren0012 <naruto_0212@yahoo.com>
2023-05-19 20:19:08 +0800

    post project fe duussan

--- fe/context/categoryContext.tsx
+++ fe/context/categoryContext.tsx
@@ -1,16 +1,37 @@
-import React, { useState } from "react";
-import { createContext } from "react";
+import React, { useEffect, useState, createContext } from "react";
+
 import { useRouter } from "next/router";
 import axios from "axios";
+import { BASE_URL } from "@/variables";
 export const CategoryContext = createContext({});
 
 const CategoryProvider = ({ children }: any) => {
   const [open, setOpen] = useState(false);
   const [subCat, setSubCat] = useState<any>([]);
+  const [post, setPost] = React.useState("");
+
   const [categoriesData, setCategoriesData] = useState<any>();
+
+  const getCategories = async () => {
+    try {
+      const res = await axios.get(`${BASE_URL}/categories`);
+      console.log("asd", res);
+      setCategoriesData(res.data.category);
+    } catch (err) {
+      console.log("err", err);
+    }
+  };
+
+  useEffect(() => {
+    console.log("asd");
+    getCategories();
+  }, []);
+
   return (
     <CategoryContext.Provider
       value={{
+        post,
+        setPost,
         subCat,
         setSubCat,
         open,
